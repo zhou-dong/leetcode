164ca3bab95a4932e117b9757a362fee11d715dd
zhou-dong <82224165@qq.com>
2016-09-07 01:12:01 -0500

    update

--- src/org/dzhou/leetcode/BalancedBinaryTree.java
+++ src/org/dzhou/leetcode/BalancedBinaryTree.java
@@ -44,4 +44,24 @@ public class BalancedBinaryTree {
 			return Math.max(left, right) + 1;
 		}
 	}
+
+	public class Solution {
+		public boolean isBalanced(TreeNode root) {
+			return height(root) != -1;
+		}
+
+		private int height(TreeNode root) {
+			if (root == null)
+				return 0;
+			int left = height(root.left);
+			if (left == -1)
+				return -1;
+			int right = height(root.right);
+			if (right == -1)
+				return -1;
+			if (Math.abs(left - right) > 1)
+				return -1;
+			return Math.max(left, right) + 1;
+		}
+	}
 }
