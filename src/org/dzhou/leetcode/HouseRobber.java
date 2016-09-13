54571903c1a44b301c3b8cb2149ca76a3682b19b
zhou-dong <82224165@qq.com>
2016-09-12 23:56:42 -0500

    update

---
+++ src/org/dzhou/leetcode/HouseRobber.java
@@ -0,0 +1,51 @@
+package org.dzhou.leetcode;
+
+import java.util.Random;
+
+/**
+ * 198. House Robber
+ * 
+ * You are a professional robber planning to rob houses along a street. Each
+ * house has a certain amount of money stashed, the only constraint stopping you
+ * from robbing each of them is that adjacent houses have security system
+ * connected and it will automatically contact the police if two adjacent houses
+ * were broken into on the same night.
+ * 
+ * Given a list of non-negative integers representing the amount of money of
+ * each house, determine the maximum amount of money you can rob tonight without
+ * alerting the police.
+ * 
+ * @author zhoudong
+ *
+ */
+public class HouseRobber {
+
+	public int rob(int[] nums) {
+		if (nums == null || nums.length == 0)
+			return 0;
+		if (nums.length == 1)
+			return nums[0];
+		if (nums.length == 2)
+			return Math.max(nums[0], nums[1]);
+		return dp(nums)[nums.length - 1];
+	}
+
+	private int[] dp(int[] nums) {
+		int[] dp = new int[nums.length];
+		dp[0] = nums[0];
+		dp[1] = nums[1];
+		for (int i = 2; i < nums.length; i++) {
+			dp[i] = Math.max(dp[i - 1], dp[i] + dp[i - 2]);
+		}
+		return dp;
+	}
+
+	public static void main(String[] args) {
+		Random random = new Random();
+
+		for (int i = 0; i < 30; i++) {
+			System.out.print(random.nextInt(20) + ", ");
+		}
+	}
+
+}
