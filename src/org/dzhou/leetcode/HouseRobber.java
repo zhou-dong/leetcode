823779dd53a6b550922accf1041e59cd7604a5da
zhou-dong <82224165@qq.com>
2016-09-13 00:11:49 -0500

    update

--- src/org/dzhou/leetcode/HouseRobber.java
+++ src/org/dzhou/leetcode/HouseRobber.java
@@ -33,19 +33,11 @@ public class HouseRobber {
 	private int[] dp(int[] nums) {
 		int[] dp = new int[nums.length];
 		dp[0] = nums[0];
-		dp[1] = nums[1];
+		dp[1] = Math.max(nums[0], nums[1]);
 		for (int i = 2; i < nums.length; i++) {
-			dp[i] = Math.max(dp[i - 1], dp[i] + dp[i - 2]);
+			dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
 		}
 		return dp;
-	}
-
-	public static void main(String[] args) {
-		Random random = new Random();
-
-		for (int i = 0; i < 30; i++) {
-			System.out.print(random.nextInt(20) + ", ");
-		}
 	}
 
 }
