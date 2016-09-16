a44a8fe311975e93832f61c8fc7cbda500b3a8fc
zhou-dong <82224165@qq.com>
2016-09-16 01:48:44 -0500

    update

---
+++ src/org/dzhou/leetcode/MaximumProductSubarray.java
@@ -0,0 +1,17 @@
+package org.dzhou.leetcode;
+
+/**
+ * 152. Maximum Product Subarray
+ * 
+ * Find the contiguous subarray within an array (containing at least one number)
+ * which has the largest product.
+ * 
+ * For example, given the array [2,3,-2,4], the contiguous subarray [2,3] has
+ * the largest product = 6.
+ * 
+ * @author zhoudong
+ *
+ */
+public class MaximumProductSubarray {
+
+}
---
+++ src/org/dzhou/leetcode/PalindromePartitioningII.java
@@ -0,0 +1,46 @@
+package org.dzhou.leetcode;
+
+/**
+ * 132. Palindrome Partitioning II
+ * 
+ * Given a string s, partition s such that every substring of the partition is a
+ * palindrome.
+ * 
+ * Return the minimum cuts needed for a palindrome partitioning of s.
+ * 
+ * For example, given s = "aab",
+ * 
+ * Return 1 since the palindrome partitioning ["aa","b"] could be produced using
+ * 1 cut.
+ * 
+ * @author zhoudong
+ *
+ *
+ */
+public class PalindromePartitioningII {
+
+	public int minCut(String s) {
+		if (s == null || s.length() == 0)
+			return 0;
+
+		int len = s.length();
+		int cuts[] = new int[len + 1];
+		// 初始化cuts里面的值为最坏情况的值
+		for (int i = 0; i < len; i++) {
+			cuts[i] = len - i;
+		}
+		boolean[][] matrix = new boolean[len][len];
+		// dynamic programming 过程
+		for (int i = len - 1; i >= 0; --i) {
+			for (int j = i; j < len; ++j) {
+				if ((s.charAt(i) == s.charAt(j) && (j - i < 2))
+						|| (s.charAt(i) == s.charAt(j) && matrix[i + 1][j - 1])) {
+					matrix[i][j] = true;
+					cuts[i] = Math.min(cuts[i], cuts[j + 1] + 1);
+				}
+			}
+		}
+		return cuts[0] - 1;
+	}
+
+}
