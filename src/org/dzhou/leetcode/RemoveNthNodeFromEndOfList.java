beb9cc8cbafd855e0f055fa799bb2f4f73d7de35
zhou-dong <82224165@qq.com>
2016-09-19 00:23:09 -0500

    update

--- src/org/dzhou/leetcode/RemoveNthNodeFromEndOfList.java
+++ src/org/dzhou/leetcode/RemoveNthNodeFromEndOfList.java
@@ -32,31 +32,28 @@ public class RemoveNthNodeFromEndOfList {
 		}
 	}
 
-	public class Fast_Slow_Solution {
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+		if (head == null || head.next == null)
+			return null;
 
-		public ListNode removeNthFromEnd(ListNode head, int n) {
-			if (head == null || head.next == null)
-				return null;
+		ListNode fast = head;
+		ListNode slow = head;
 
-			ListNode fast = head;
-			ListNode slow = head;
+		for (int i = 0; i < n; i++) {
+			fast = fast.next;
+		}
 
-			for (int i = 0; i < n; i++) {
-				fast = fast.next;
-			}
-
-			if (fast == null) {
-				head = head.next;
-				return head;
-			}
-			while (fast.next != null) {
-				fast = fast.next;
-				slow = slow.next;
-			}
-
-			slow.next = slow.next.next;
+		if (fast == null) {
+			head = head.next;
 			return head;
 		}
+		while (fast.next != null) {
+			fast = fast.next;
+			slow = slow.next;
+		}
+
+		slow.next = slow.next.next;
+		return head;
 
 	}
 
