8a94601ef03ae88ddb6ba75fdd899cb05c9f5c73
zhou-dong <82224165@qq.com>
2016-09-25 04:10:00 -0500

    update

--- src/org/dzhou/leetcode/SearchInsertPosition.java
+++ src/org/dzhou/leetcode/SearchInsertPosition.java
@@ -29,11 +29,11 @@ public class SearchInsertPosition {
 		int low = 0, high = nums.length - 1;
 		while (low <= high) {
 			int mid = (low + high) / 2;
-			if (target > nums[mid])
+			if (target > nums[mid]) {
 				low = mid + 1;
-			else if (target < nums[mid])
+			} else if (target < nums[mid]) {
 				high = mid - 1;
-			else {
+			} else {
 				return mid;
 			}
 		}
