2d80677d660eeb89c840b43cce38016486e4a871
zhou-dong <82224165@qq.com>
2016-09-25 00:09:23 -0500

    update

---
+++ src/org/dzhou/leetcode/ZigZagConversion.java
@@ -0,0 +1,28 @@
+package org.dzhou.leetcode;
+
+/**
+ * 6. ZigZag Conversion
+ * 
+ * @author zhoudong
+ *
+ */
+public class ZigZagConversion {
+
+	public String convert(String s, int numRows) {
+		if (numRows < 2 || s == null || s.length() == 0) {
+			return s;
+		}
+		int size = 2 * (numRows - 1);
+		StringBuilder rst = new StringBuilder();
+		for (int i = 0; i < numRows; i++) {
+			for (int j = i; j < s.length(); j += size) {
+				rst.append(s.charAt(j));
+				if (i != 0 && i != numRows - 1 && j + size - 2 * i < s.length()) {
+					rst.append(s.charAt(j + size - 2 * i));
+				}
+			}
+		}
+		return rst.toString();
+	}
+
+}
