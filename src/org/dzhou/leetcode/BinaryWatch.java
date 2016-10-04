155f667ae5d67a64f2be943d563a8c97824d8591
zhou-dong <82224165@qq.com>
2016-10-03 21:24:08 -0500

    update

--- src/org/dzhou/leetcode/BinaryWatch.java
+++ src/org/dzhou/leetcode/BinaryWatch.java
@@ -39,13 +39,10 @@ public class BinaryWatch {
 
 	public List<String> readBinaryWatch(int num) {
 		List<String> list = new ArrayList<>();
-		// 用来表示时间的所有可能的取值
-		int timecode[] = new int[10];
-		dfs(timecode, 0, 0, list, num);
+		dfs(new int[10], 0, 0, list, num);
 		return list;
 	}
 
-	// dfs 遍历所有可能性
 	private void dfs(int[] timecode, int i, int k, List<String> list, int num) {
 		if (k == num) {
 			String res = decodeToTime(timecode);
@@ -53,8 +50,9 @@ public class BinaryWatch {
 				list.add(res);
 			return;
 		}
-		if (i == timecode.length)
+		if (i == timecode.length) {
 			return;
+		}
 		timecode[i] = 1;
 		dfs(timecode, i + 1, k + 1, list, num);
 		timecode[i] = 0;
@@ -64,24 +62,26 @@ public class BinaryWatch {
 	// 输出时间，即输出可能的时间，要是时间不对则输出null
 	private String decodeToTime(int[] timecode) {
 		int hours = 0;
-		// 按照位数转换时间
 		for (int i = 0; i < 4; i++) {
 			if (timecode[i] == 1) {
-				hours = hours + (int) Math.pow(2, i);
+				hours += (int) Math.pow(2, i);
 			}
 		}
 		int minutes = 0;
 		for (int i = 4; i < 10; i++) {
 			if (timecode[i] == 1) {
-				minutes = minutes + (int) Math.pow(2, i - 4);
+				minutes += (int) Math.pow(2, i - 4);
 			}
 		}
-		String min = "" + minutes;
-		if (minutes < 10)
-			min = "0" + min;
-		// 判断时间的可行性
-		if (hours >= 12 || minutes >= 60)
-			return null;
-		return hours + ":" + min;
+		return (hours >= 12 || minutes >= 60) ? null : time(hours, minutes);
 	}
+
+	private String time(int hours, int minutes) {
+		return hours + ":" + minute(minutes);
+	}
+
+	private String minute(int minutes) {
+		return minutes < 10 ? "0" + minutes : "" + minutes;
+	}
+
 }
