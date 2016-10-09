4bbf9a19dd562fdbe9284b9720ba90ae740e2822
zhou-dong <82224165@qq.com>
2016-10-08 21:07:28 -0500

    update

---
+++ src/org/dzhou/leetcode/LargestBstSubtree.java
@@ -0,0 +1,26 @@
+package org.dzhou.leetcode;
+
+/**
+ * 333. Largest BST Subtree
+ * 
+ * Given a binary tree, find the largest subtree which is a Binary Search Tree
+ * (BST), where largest means subtree with largest number of nodes in it.
+ * 
+ * Note: A subtree must include all of its descendants. Here's an example:
+ * 
+ * @author zhoudong
+ *
+ */
+public class LargestBstSubtree {
+
+	// Definition for a binary tree node.
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+}
