d90e789add98dc8e5e7f88d21935d9b65e721b13
zhou-dong <82224165@qq.com>
2017-01-22 20:55:55 -0600

    update

---
+++ src/org/dzhou/other/HotelElevator.java
@@ -0,0 +1,50 @@
+package org.dzhou.other;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class HotelElevator {
+	public int solution(int[] A, int[] B, int M, int X, int Y) {
+		// write your code in Java SE 8
+		Queue<Person> queue = new LinkedList<>();
+		for (int i = 0; i < A.length; i++) {
+			queue.add(new Person(A[i], B[i]));
+		}
+
+		int result = 0;
+
+		int count = 0, weights = 0;
+		Set<Integer> targets = new HashSet<>();
+		while (!queue.isEmpty()) {
+			if (count == X || (weights + queue.peek().weight) > Y) {
+				result += (targets.size() + 1);
+				count = 0;
+				weights = 0;
+				targets.clear();
+			}
+			Person person = queue.poll();
+			count++;
+			weights += person.weight;
+			targets.add(person.target);
+		}
+
+		if (count != 0) {
+			result += (targets.size() + 1);
+		}
+
+		return result;
+	}
+
+	class Person {
+		int weight;
+		int target;
+
+		Person(int w, int t) {
+			this.weight = w;
+			this.target = t;
+		}
+	}
+
+}
---
+++ src/org/dzhou/other/ParkingFee.java
@@ -0,0 +1,28 @@
+package org.dzhou.other;
+
+public class ParkingFee {
+
+	public int solution(String E, String L) {
+		// write your code in Java SE 8
+		int total = 2 + 3;
+		int parkingHour = getParkingHour(toTime(E), toTime(L));
+		if (parkingHour > 1) {
+			total += 4 * (parkingHour - 1);
+		}
+		return total;
+	}
+
+	private int getParkingHour(int[] entry, int[] left) {
+		int total = left[0] - entry[0];
+		int entra = (left[1] - entry[1] > 0) ? 1 : 0;
+		return total + entra;
+	}
+
+	private int[] toTime(String str) {
+		String[] array = str.split(":");
+		int hour = Integer.parseInt(array[0]);
+		int minute = Integer.parseInt(array[1]);
+		return new int[] { hour, minute };
+	}
+
+}
