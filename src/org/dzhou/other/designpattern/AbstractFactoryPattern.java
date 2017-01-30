1e3e12523f9d402a3ca1eaae22ffd7ff45c87550
zhou-dong <82224165@qq.com>
2017-01-30 02:09:43 -0600

    abstract factory pattern

--- src/org/dzhou/other/designpattern/AbstractFactoryPattern.java
+++ src/org/dzhou/other/designpattern/AbstractFactoryPattern.java
@@ -2,4 +2,146 @@ package org.dzhou.other.designpattern;
 
 public class AbstractFactoryPattern {
 
+	public static abstract class Auto {
+		private double price;
+		private String brand;
+		private String color;
+
+		public abstract void makeAuto();
+
+		public double getPrice() {
+			return price;
+		}
+
+		public void setPrice(double price) {
+			this.price = price;
+		}
+
+		public String getBrand() {
+			return brand;
+		}
+
+		public void setBrand(String brand) {
+			this.brand = brand;
+		}
+
+		public String getColor() {
+			return color;
+		}
+
+		public void setColor(String color) {
+			this.color = color;
+		}
+	}
+
+	public static class BMW extends Auto {
+		AutoFactory factory;
+
+		public BMW(AutoFactory factory) {
+			this.factory = factory;
+		}
+
+		@Override
+		public void makeAuto() {
+			setColor(factory.addColor());
+			setBrand(factory.addBrand());
+			setPrice(factory.addPrice());
+		}
+	}
+
+	public static class DODGE extends Auto {
+		AutoFactory factory;
+
+		public DODGE(AutoFactory factory) {
+			this.factory = factory;
+		}
+
+		@Override
+		public void makeAuto() {
+			setColor(this.factory.addColor());
+			setBrand(this.factory.addBrand());
+			setPrice(this.factory.addPrice());
+		}
+	}
+
+	interface AutoFactory {
+		public String addBrand();
+
+		public String addColor();
+
+		public double addPrice();
+	}
+
+	public static class BMWFactory implements AutoFactory {
+		@Override
+		public String addBrand() {
+			return "BMW";
+		}
+
+		@Override
+		public String addColor() {
+			return "BROWN";
+		}
+
+		@Override
+		public double addPrice() {
+			return 30000.00;
+		}
+	}
+
+	public static class DODGEFactory implements AutoFactory {
+		@Override
+		public String addBrand() {
+			return "DODGE";
+		}
+
+		@Override
+		public String addColor() {
+			return "RED";
+		}
+
+		@Override
+		public double addPrice() {
+			return 20000.00;
+		}
+	}
+
+	public static abstract class AutoBuilding {
+		protected abstract Auto makeAuto(String typeOfAuto);
+
+		public Auto orderAuto(String typeOfAuto) {
+			Auto auto = makeAuto(typeOfAuto);
+			auto.makeAuto();
+			return auto;
+		}
+	}
+
+	public static class BasicAutoBuilding extends AutoBuilding {
+		@Override
+		protected Auto makeAuto(String typeOfAuto) {
+			switch (typeOfAuto) {
+			case "BMW":
+				return new BMW(new BMWFactory());
+			case "DODGE":
+				return new DODGE(new DODGEFactory());
+			}
+			return null;
+		}
+	}
+
+	public static void main(String[] args) {
+		AutoBuilding basicAutoBuilding = new BasicAutoBuilding();
+		Auto bmw = basicAutoBuilding.orderAuto("BMW");
+		Auto dodge = basicAutoBuilding.orderAuto("DODGE");
+		print(bmw);
+		print(dodge);
+	}
+
+	private static void print(Auto auto) {
+		System.out.println("brand: " + auto.getBrand());
+		System.out.println("color: " + auto.getColor());
+		System.out.println("price: " + auto.getPrice());
+		System.out.println();
+	}
+
 }
