e0ad3258cc9df3365b5f0ac4c823e05aac7214dc
zhou-dong <82224165@qq.com>
2017-01-30 03:09:04 -0600

    builder pattern

---
+++ src/org/dzhou/other/designpattern/BuilderPattern.java
@@ -0,0 +1,59 @@
+package org.dzhou.other.designpattern;
+
+public class BuilderPattern {
+
+	public static class Auto {
+		double price;
+		String brand;
+		String color;
+		String country;
+		int engines;
+	}
+
+	public static class AutoBuilder {
+
+		public static AutoBuilder Builder(String brand, double price) {
+			return new AutoBuilder(brand, price);
+		}
+
+		Auto auto = null;
+
+		private AutoBuilder() {
+		}
+
+		private AutoBuilder(String brand, double price) {
+			auto = new Auto();
+			auto.brand = brand;
+			auto.price = price;
+		}
+
+		public AutoBuilder setColor(String color) {
+			auto.color = color;
+			return this;
+		}
+
+		public AutoBuilder setCountry(String country) {
+			auto.country = country;
+			return this;
+		}
+
+		public AutoBuilder setEngines(int engines) {
+			auto.engines = engines;
+			return this;
+		}
+
+		public Auto build() {
+			return auto;
+		}
+	}
+
+	public static void main(String[] args) {
+		Auto bmw = AutoBuilder.Builder("BMW", 50000.00).setColor("Black").setEngines(8).setCountry("Germany").build();
+		System.out.println(bmw.brand);
+		System.out.println(bmw.price);
+		System.out.println(bmw.color);
+		System.out.println(bmw.country);
+		System.out.println(bmw.engines);
+	}
+
+}
