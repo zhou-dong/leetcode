331619d92b9b7a77a96f6bff851eb09afd9657a2
zhou-dong <82224165@qq.com>
2017-02-03 00:13:24 -0600

    basic mul thread

--- src/org/dzhou/other/multhreading/Basic.java
+++ src/org/dzhou/other/multhreading/Basic.java
@@ -1,5 +1,46 @@
 package org.dzhou.other.multhreading;
 
-public class Basic {
+class RunnerThreand extends Thread {
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(Thread.currentThread() + " " + i);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
 
+class RunnerRunnable implements Runnable {
+	@Override
+	public void run() {
+		for (int i = 11; i < 20; i++) {
+			System.out.println(Thread.currentThread() + " " + i);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+public class Basic {
+	public static void main(String[] args) {
+		new RunnerThreand().start();
+		new RunnerThreand().start();
+
+		new Thread(new RunnerRunnable()).start();
+		new Thread(new RunnerRunnable()).start();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("hello world...");
+			}
+		}).start();
+	}
 }
