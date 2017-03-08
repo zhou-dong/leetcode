42614def46e41d61130c03badc8741c0c7db10c8
zhou-dong <82224165@qq.com>
2017-03-08 06:32:46 -0600

    update

---
+++ src/org/dzhou/leetcode/EncodeAndDecodeTinyURL.java
@@ -0,0 +1,37 @@
+package org.dzhou.leetcode;
+
+/**
+ * 535. Encode and Decode TinyURL
+ * 
+ * TinyURL is a URL shortening service where you enter a URL such as
+ * https://leetcode.com/problems/design-tinyurl and it returns a short URL such
+ * as http://tinyurl.com/4e9iAk.
+ * 
+ * Design the encode and decode methods for the TinyURL service. There is no
+ * restriction on how your encode/decode algorithm should work. You just need to
+ * ensure that a URL can be encoded to a tiny URL and the tiny URL can be
+ * decoded to the original URL.
+ * 
+ * @author zhoudong
+ *
+ */
+public class EncodeAndDecodeTinyURL {
+
+	public class Codec {
+
+		// Encodes a URL to a shortened URL.
+		public String encode(String longUrl) {
+
+		}
+
+		// Decodes a shortened URL to its original URL.
+		public String decode(String shortUrl) {
+
+		}
+	}
+
+	// Your Codec object will be instantiated and called as such:
+	// Codec codec = new Codec();
+	// codec.decode(codec.encode(url));
+
+}
