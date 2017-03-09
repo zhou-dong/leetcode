5fb202b7d94ca22d33b95992b00d34fa8ffc530e
zhou-dong <82224165@qq.com>
2017-03-08 21:27:59 -0600

    update

--- src/org/dzhou/leetcode/Minesweeper.java
+++ src/org/dzhou/leetcode/Minesweeper.java
@@ -1,5 +1,8 @@
 package org.dzhou.leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 529. Minesweeper
  * 
@@ -7,45 +10,75 @@ package org.dzhou.leetcode;
  *
  */
 public class Minesweeper {
+
 	public char[][] updateBoard(char[][] board, int[] click) {
-		int m = board.length, n = board[0].length;
 		int row = click[0], col = click[1];
 
-		if (board[row][col] == 'M') { // Mine
+		if (board[row][col] == 'M') {
 			board[row][col] = 'X';
-		} else { // Empty
-					// Get number of mines first.
-			int count = 0;
-			for (int i = -1; i < 2; i++) {
-				for (int j = -1; j < 2; j++) {
-					if (i == 0 && j == 0)
-						continue;
-					int r = row + i, c = col + j;
-					if (r < 0 || r >= m || c < 0 || c < 0 || c >= n)
-						continue;
-					if (board[r][c] == 'M' || board[r][c] == 'X')
-						count++;
-				}
-			}
-
-			if (count > 0) { // If it is not a 'B', stop further DFS.
-				board[row][col] = (char) (count + '0');
-			} else { // Continue DFS to adjacent cells.
-				board[row][col] = 'B';
-				for (int i = -1; i < 2; i++) {
-					for (int j = -1; j < 2; j++) {
-						if (i == 0 && j == 0)
-							continue;
-						int r = row + i, c = col + j;
-						if (r < 0 || r >= m || c < 0 || c < 0 || c >= n)
-							continue;
-						if (board[r][c] == 'E')
-							updateBoard(board, new int[] { r, c });
-					}
-				}
-			}
+			return board;
 		}
 
+		int count = countAdjacentMine(board, row, col);
+		if (count > 0) { // If it is not a 'B', stop further DFS.
+			board[row][col] = (char) (count + '0');
+			return board;
+		}
+
+		// Continue DFS to adjacent cells.
+		board[row][col] = 'B';
+		for (int[] direction : directions()) {
+			int[] next = { row + direction[0], col + direction[1] };
+			if (!isValid(board, next)) {
+				continue;
+			}
+			if (board[next[0]][next[1]] == 'E') {
+				updateBoard(board, next);
+			}
+		}
 		return board;
 	}
+
+	private int countAdjacentMine(char[][] board, int row, int col) {
+		int count = 0;
+		for (int[] direction : directions()) {
+			if (isMine(board, row + direction[0], col + direction[1])) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	private Set<int[]> directions() {
+		Set<int[]> directions = new HashSet<>();
+		for (int row = -1; row < 2; row++) {
+			for (int col = -1; col < 2; col++) {
+				if (row == 0 && col == 0) {
+					continue;
+				}
+				directions.add(new int[] { row, col });
+			}
+		}
+		return directions;
+	}
+
+	private boolean isMine(char[][] board, int row, int col) {
+		if (!isValid(board, row, col)) {
+			return false;
+		}
+		if (board[row][col] == 'M' || board[row][col] == 'X') {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private boolean isValid(char[][] board, int[] position) {
+		return isValid(board, position[0], position[1]);
+	}
+
+	private boolean isValid(char[][] board, int row, int col) {
+		return row >= 0 && col >= 0 && row < board.length && col < board[row].length;
+	}
+
 }
