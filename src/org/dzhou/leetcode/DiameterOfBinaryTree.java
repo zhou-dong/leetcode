9824d6f4fef34f27ec47b280b633b8d221603fa6
zhou-dong <82224165@qq.com>
2017-03-28 23:05:46 -0500

    update

--- src/org/dzhou/leetcode/DiameterOfBinaryTree.java
+++ src/org/dzhou/leetcode/DiameterOfBinaryTree.java
@@ -42,4 +42,22 @@ public class DiameterOfBinaryTree {
 
 	}
 
+	public class solution1 {
+
+		int max = 0;
+
+		public int diameterOfBinaryTree(TreeNode root) {
+			maxDepth(root);
+			return max;
+		}
+
+		private int maxDepth(TreeNode root) {
+			if (root == null)
+				return 0;
+			int left = maxDepth(root.left);
+			int right = maxDepth(root.right);
+			max = Math.max(max, left + right);
+			return Math.max(left, right) + 1;
+		}
+	}
 }
